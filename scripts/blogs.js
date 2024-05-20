c47896cf5cb5d2de4af244c9fb7d2b900d74091f
Harsimran Virk <i.harsimranvirk@gmail.com>
2024-05-21 02:43:03 +0530

    Added vite

--- scripts/blogs.js
+++ scripts/blogs.js
@@ -1,22 +1,21 @@
-fetch("../blogs.json")
-.then(res => res.json())
-.then(blogs => {
-    const blogsList = blogs["blogs"]
-    const blogsContainer = document.getElementById("blogs-container")
-    blogsList.forEach(blog => {
-        const div = document.createElement("div")
-        const img = document.createElement("img")
-        const text = document.createElement("div")
+import blogs from '../blogs.json'
 
-        div.className = "blog-card"
-        div.style.backgroundColor = blog.bgColor
-        div.onclick = e => location.href = blog.link
-        img.src = blog.imgSrc
-        img.className = "blog-card-img"
-        text.innerText = blog.text
+const blogsContainer = document.getElementById("blogs-container")
+const blogsList = blogs["blogs"]
+    
+blogsList.forEach(blog => {
+    const div = document.createElement("div")
+    const img = document.createElement("img")
+    const text = document.createElement("div")
 
-        div.appendChild(img)
-        div.appendChild(text)
-        blogsContainer.appendChild(div)
-    })
+    div.className = "blog-card"
+    div.style.backgroundColor = blog.bgColor
+    div.onclick = e => location.href = blog.link
+    img.src = blog.imgSrc
+    img.className = "blog-card-img"
+    text.innerText = blog.text
+
+    div.appendChild(img)
+    div.appendChild(text)
+    blogsContainer.appendChild(div)
 })
---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import { resolve } from 'path'
+import blogs from './blogs.json'
+
+const blogsList = blogs.blogs
+const pages = blogsList.reduce((a, b) => ({ ...a, [b.link]: resolve(__dirname, b.link) }), {})
+const input = Object.assign({}, { home: resolve(__dirname, "index.html") }, pages)
+console.log(input)
+
+export default defineConfig({
+    build: {
+        rollupOptions: {
+            input
+        }
+    }
+})
